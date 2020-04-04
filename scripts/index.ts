d1b659135bc6d9f8d1019b72ec1f503a805968b1
Stephen <stephen.c.weiss@gmail.com>
2020-04-04 18:12:10 -0500

    feat: extract local_data to constant
    this will then make it easier to use a .env

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,8 +1,10 @@
 import path from "path";
 import { getFiles, filesFilter } from "./helpers";
 
+const LOCAL_DATA = process.env.local_data || path.resolve(__dirname, "../../data/localData")
+
 function processLocalData() {
-  const files = getFiles(path.resolve(__dirname, "../../data/localData"));
+  const files = getFiles(LOCAL_DATA);
   console.log(files)
   const excelFiles = filesFilter(files, 'xlsx')
   console.log(excelFiles);
